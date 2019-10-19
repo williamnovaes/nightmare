df75150d0c76faa15a21fb0fed15845647823102
William Fermino <william.fermino@nxmultiservicos.com.br>
2019-10-18 22:19:52 -0300

    movimentacao bat, prefab bat

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,32 +7,37 @@ public class EnemyMovement : MonoBehaviour
     
     Rigidbody2D enemyRB;
 
-    [SerializeField] float moveSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private bool facingRigth;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyRB = GetComponent<Rigidbody2D>();
+        time = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         enemyRB.velocity = new Vector2(moveSpeed, 0f);
+        if (time >= 2f)
+        {
+            Flip();
+            time = 0f;
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+
     }
 
     private void Flip()
     {
         moveSpeed *= -1;
-        transform.localScale = new Vector2(Mathf.Sign(enemyRB.velocity.x), transform.localScale.y);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Colidiu " + collision.gameObject.name);
-        if (collision.gameObject.layer.Equals("Foreground"))
-        {
-            Flip();
-        }
+        facingRigth = !facingRigth;
+        transform.localScale = new Vector2(moveSpeed, transform.localScale.y);
     }
 }
